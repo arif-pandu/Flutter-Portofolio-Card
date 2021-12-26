82225df8986cd2ef1ebc97c257ac1d9a91179c69
arif-pandu <mapanduprayoga@gmail.com>
2021-12-26 13:34:17 +0700

    REMAKE

---
+++ lib/homepage.dart
@@ -0,0 +1,178 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:potofolio_web/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: whiteColor,
+      // appBar: AppBar(title: Text('Draw Line')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomPaint(
+            painter: LinePainter(),
+            child: Container(
+              height: height / 3,
+              width: width,
+              decoration: BoxDecoration(
+                  // border: Border.all(width: 2, color: blackColor),
+                  ),
+              child: Stack(
+                children: [
+                  Center(
+                    child: Text(
+                      'MAPENPROJECT@GMAIL.COM',
+                      style: TextStyle(
+                        fontFamily: 'Helvetica',
+                        fontSize: 22,
+                        color: blackColor,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: width / 2 - 20,
+                    top: height / 3 / 5 - 20,
+                    child: GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor: whiteColor,
+                          barrierColor: blackColor.withOpacity(0.5),
+                          enableDrag: true,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          context: context,
+                          builder: (context) {
+                            return Container(
+                              height: height,
+                            );
+                          },
+                        );
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: whiteColor,
+                          border: Border.all(width: 1, color: blackColor),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // PAINT VAR
+    var paint = Paint()
+      ..color = blackColor
+      ..strokeWidth = 1.5
+      ..style = PaintingStyle.stroke;
+    //Button EMAIL
+    var path3 = Path()
+      ..moveTo(size.width / 10 - 15, size.height / 3 + 15)
+      ..lineTo(size.width / 10 - 15, size.height / 3)
+      ..lineTo(size.width * 0.9 + 15, size.height / 3)
+      ..lineTo(size.width * 0.9 + 15, size.height / 3 + 15);
+
+    var path3Button = Path()
+      ..moveTo(size.width / 2, size.height / 3)
+      ..lineTo(size.width / 2, size.height / 5);
+
+    canvas.drawPath(path3, paint);
+    canvas.drawPath(path3Button, paint);
+    // Button Social
+    var path1 = Path()
+      ..moveTo(size.width / 10 - 15, size.height - (size.height / 3 + 15))
+      ..lineTo(size.width / 10 - 15, size.height - (size.height / 3))
+      ..lineTo(size.width / 4 + 5, size.height - (size.height / 3))
+      ..lineTo(size.width / 4 + 5, size.height - (size.height / 3 + 15));
+    var path1Button = Path()
+      ..moveTo(
+          size.width / 10 -
+              15 +
+              (((size.width / 4 + 5) - (size.width / 10 - 15)) / 2),
+          size.height - (size.height / 3))
+      ..lineTo(
+          size.width / 10 -
+              15 +
+              (((size.width / 4 + 5) - (size.width / 10 - 15)) / 2),
+          size.height - (size.height / 3 - 30));
+
+    canvas.drawPath(path1, paint);
+    canvas.drawPath(path1Button, paint);
+    // Button Project
+    var path2 = Path()
+      ..moveTo(size.width / 4 + 10, size.height - (size.height / 3 + 15))
+      ..lineTo(size.width / 4 + 10, size.height - (size.height / 3))
+      ..lineTo(size.width / 2 + 12, size.height - (size.height / 3))
+      ..lineTo(size.width / 2 + 12, size.height - (size.height / 3 + 15));
+
+    var path2Button = Path()
+      ..moveTo(
+          (size.width / 4 + 10) +
+              (((size.width / 2 + 12) - (size.width / 4 + 10)) / 2),
+          size.height - (size.height / 3))
+      ..lineTo(
+          (size.width / 4 + 10) +
+              (((size.width / 2 + 12) - (size.width / 4 + 10)) / 2),
+          size.height - (size.height / 3 - 30));
+
+    canvas.drawPath(path2, paint);
+    canvas.drawPath(path2Button, paint);
+
+    // Button Website
+    var path4 = Path()
+      ..moveTo(size.width * 0.9 + 15, size.height - (size.height / 3 + 15))
+      ..lineTo(size.width * 0.9 + 15, size.height - (size.height / 3))
+      ..lineTo(size.width * 0.9 - 30, size.height - (size.height / 3))
+      ..lineTo(size.width * 0.9 - 30, size.height - (size.height / 3 + 15));
+
+    var path4Button = Path()
+      ..moveTo(
+          (size.width * 0.9 - 30) +
+              (((size.width * 0.9 + 15) - (size.width * 0.9 - 30)) / 2),
+          size.height - (size.height / 3))
+      ..lineTo(
+          (size.width * 0.9 - 30) +
+              (((size.width * 0.9 + 15) - (size.width * 0.9 - 30)) / 2),
+          size.height - (size.height / 3 - 30));
+
+    canvas.drawPath(path4, paint);
+    canvas.drawPath(path4Button, paint);
+  }
+
+  //
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color greyColor = Color(0xffb3b3b3);
+const Color blueColor = Color(0xff3d44ef);
+const Color blackColor = Color(0xff1a1a1a);
+const Color redColor = Color(0xffff0000);
+const Color orangeColor = Color(0xffff6600);
+const Color greenColor = Color(0xff198817);
+const Color yellowColor = Color(0xffffc700);
+const Color whiteColor = Color(0xffffffff);
+
+const TextStyle styleContent =
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
